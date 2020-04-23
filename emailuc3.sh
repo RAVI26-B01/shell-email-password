76dc83ba3cde79946853cf0955a613bc7de7209d
RAVI26-B01 <ravi26.b01@gmail.com>
2020-04-23 07:49:50 +0530

    . and the 3rd part

---
+++ emailuc3.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+shopt -s extglob
+echo "enter email"
+read email
+
+pat="^[a-zA-Z0-9]+[.]*[a-zA-Z0-9]*[@][a-zA-Z0-9]+[.][a-zA-Z]{2}"
+
+if [[ $email =~ $pat ]]
+then
+        echo "yes"
+else
+        echo "no"
+fi
